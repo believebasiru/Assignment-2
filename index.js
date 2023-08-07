d011d5fa8074d957d6257b3b289b8ecdb79fa1e4
Alege Emmanuel Oluwasegun <emmaalege@gmail.com>
2023-08-07 16:53:32 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Challenge 1
+
+// Add the following elements to the container class in the HTML file using ONLY JavaScript and the DOM methods.
+
+// 1. a <p> with red text that says “Hey I’m red!”
+// 2. an <h3> with blue text that says “I’m a blue h3!”
+// 3. a <div> with a black border and pink background color with the following elements inside of it:
+//     i. another <h1> that says “I’m in a div”
+//     ii. a <p> that says “ME TOO!”
+
+
+// Challenge 2
+// Write a JavaScript function that takes an array of numbers and finds the lowest and highest numbers.
+
+
+// Challenge 3
+// Write a JavaScript program to compute the sum and product of an array of integers
+
+
+// Challenge 4
+//  Write a Javascript program that changes the background color of the Click Me button to red and 
+// also changes its text color to grey at the click of the button.
+
+// Challenge 5
+// Write a javascript program that calculates a simultaneous equation using the almighty formula(quadratic formula).
+// Hint: Known what the almighty formula, pass in the a,b,c of the almighty formula as a parameter
